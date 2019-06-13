db33a651bd1f3b92364fcdd4fa4a6e465a384dc2
Matisse <matisse.laurel@borngroup.com>
2019-06-13 12:23:09 -0700

    test

--- registration.php
+++ registration.php
@@ -3,6 +3,6 @@
 
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::THEME,
-    'frontend/Starbucksb2b/sbux',
+    'frontend/Matisseborn/sbux',
     __DIR__
 );
